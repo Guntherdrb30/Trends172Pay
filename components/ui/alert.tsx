629df11ae6ef3fd5a2a0aa22dc61691a1f4ef2fa
carpihogarai <carpihogar.ai@gmail.com>
2025-12-06 14:14:08 -0800

    Fix: Add missing AlertDescription export

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -17,3 +17,27 @@ export function Alert({
   );
 }
 
+export function AlertTitle({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h5
+      className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+      {...props}
+    />
+  )
+}
+
+export function AlertDescription({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLParagraphElement>) {
+  return (
+    <div
+      className={cn("text-sm [&_p]:leading-relaxed", className)}
+      {...props}
+    />
+  )
+}
+
